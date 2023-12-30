7cd60349be09a6c878ff76f90c25e00c46b4ffcb
Simran Bhandari <simranbhandari036@gmail.com>
2023-12-30 18:15:11 +0530

    Home UI & Product Card

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,31 @@
-
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
 
 const Home = () => {
+  const addToCartHandler = () => {} ;
+  
   return (
-    <div>home</div>
+    <div className="home">
+      <section></section>
+
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+          <ProductCard
+            productId="adasdasd"
+            name="Macbook"
+            price={4545}
+            stock={435}
+            handler={addToCartHandler}
+            photo="https://m.media-amazon.com/images/I/71TPda7cwUL._SX522_.jpg"
+          />
+      </main>
+    </div>
   )
 }
 
